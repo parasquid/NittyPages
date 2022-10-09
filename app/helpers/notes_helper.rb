d6d85f0378d993db2f897bdf9e77682d4eca1559
parasquid <tristan.gomez@gmail.com>
2022-10-10 03:03:25 +0800

    refactor classes so they are in just one call

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,15 +1,26 @@
 module NotesHelper
   def has_notes_class(date) = Note.where(date: date).exists? ? "font-bold" : nil
   def today_class(date) = Date.today == date ? "text-red-500" : nil
-  def current_date_class(date, start_date_param) = date == Date.parse(start_date_param) ? "bg-amber-200" : nil
+  def current_date_class(date, start_date) = date == start_date ? "bg-amber-200" : nil
+  def hover_class(date, start_date) = date != start_date ? "hover:bg-sky-300" : nil
 
-  def prev_month_class(date, start_date_param)
-    start_date = Date.parse(start_date_param)
+  def prev_month_class(date, start_date)
     (start_date.month != date.month && date < start_date) ? "bg-gray-200" : nil
   end
 
-  def next_month_class(date, start_date_param)
-    start_date = Date.parse(start_date_param)
+  def next_month_class(date, start_date)
     (start_date.month != date.month && date > start_date) ? "bg-gray-200" : nil
+  end
+
+  def date_classes(date, start_date_param)
+    start_date = Date.parse(start_date_param)
+    <<~CLASSES
+      #{today_class(date)}
+      #{has_notes_class(date)}
+      #{next_month_class(date, start_date)}
+      #{prev_month_class(date, start_date)}
+      #{current_date_class(date, start_date)}
+      #{hover_class(date, start_date)}
+    CLASSES
   end
 end
