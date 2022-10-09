16de59a429bc0dc04fa67af7dee56500d2b442de
parasquid <tristan.gomez@gmail.com>
2022-10-10 02:27:56 +0800

    add in a few helpers
    this simplifies the view by extracting the conditionals for rendering
    a particular class

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,2 +1,14 @@
 module NotesHelper
+  def has_notes_class(date) = Note.where(date: date).exists? ? "font-bold" : nil
+  def today_class(date) = Date.today == date ? "text-red-500" : nil
+
+  def prev_month_class(date, start_date_param)
+    start_date = Date.parse(start_date_param)
+    (start_date.month != date.month && date < start_date) ? "bg-gray-200" : nil
+  end
+
+  def next_month_class(date, start_date_param)
+    start_date = Date.parse(start_date_param)
+    (start_date.month != date.month && date > start_date) ? "bg-gray-200" : nil
+  end
 end
