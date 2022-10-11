de9df2104af3eb8a7f5294705efd25b66d9fee72
parasquid <tristan.gomez@gmail.com>
2022-10-11 23:34:31 +0800

    add done checklist items
    also change the default sigil for todo since i found i was using > for
    "quoted" content (might have to change again later)

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,8 +3,24 @@ class Note < ApplicationRecord
 
   def to_param() = date
 
-  def todos(sigil: ENV.fetch("SIGIL_TODO") { ">" })
+  def todos(sigil: ENV.fetch("SIGIL_TODO") { "=" })
     Rails.cache.fetch([self, :todos]) do
+      return [] unless persisted?
+      body
+        .each_line
+        .map.with_index { |line, number|
+          next unless line.start_with? "#{sigil} "
+          {
+            line: line.chomp.gsub(sigil, ""),
+            number: number
+          }
+        }
+        .compact
+      end
+  end
+
+  def dones(sigil: ENV.fetch("SIGIL_DONE") { "~" })
+    Rails.cache.fetch([self, :dones]) do
       return [] unless persisted?
       body
         .each_line
